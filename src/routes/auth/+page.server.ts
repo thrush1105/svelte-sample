9629429a61ae8a00eed368b0542aafdf0fb5080b
thrush1105 <thrush1105@gmail.com>
2025-05-06 01:34:15 +0900

    VERCEL_URLを使うように修正

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -6,6 +6,7 @@ const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ??
     'http://localhost:3000/';
   // Make sure to include `https://` when not localhost.
   url = url.startsWith('http') ? url : `https://${url}`;
