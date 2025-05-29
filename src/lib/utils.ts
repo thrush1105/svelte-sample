6adcd569ffaf0750a003fcf6b59cdae7d8ea2079
thrush1105 <thrush1105@gmail.com>
2025-05-29 22:14:36 +0900

    アプリケーションURLの実装を修正

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,14 +5,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getAppUrl = () => {
+export const getAppUrl = (fallback: string = 'http://localhost:3000') => {
   const env = process?.env?.VERCEL_ENV;
 
+  let url;
+
   if (env === 'production') {
-    return process?.env.VERCEL_PROJECT_PRODUCTION_URL;
+    url = process?.env.VERCEL_PROJECT_PRODUCTION_URL ?? fallback;
   } else if (env === 'preview') {
-    return process?.env.VERCEL_BRANCH_URL;
+    url = process?.env.VERCEL_BRANCH_URL ?? fallback;
   } else {
-    return process?.env?.VERCEL_URL;
+    url = fallback;
   }
+
+  url = url.startsWith('http') ? url : `https://${url}`;
+
+  url = url.endsWith('/') ? url : `${url}/`;
+
+  return url;
 };
